3ce3d6d5da9f94cfe845c699c42246d63113ef7e
Michael Kirsch <mkirsch.git@gmail.com>
2024-10-24 18:50:33 +0200

    TAH for HZD

--- AdressLogger/AdressLogger/Form1.Designer.cs
+++ AdressLogger/AdressLogger/Form1.Designer.cs
@@ -32,12 +32,12 @@ namespace AdressLogger
             this.components = new System.ComponentModel.Container();
             this.labelPos = new System.Windows.Forms.Label();
             this.timerMemory = new System.Windows.Forms.Timer(this.components);
-            this.buttonLogArm = new System.Windows.Forms.Button();
+            this.buttonLogStuff = new System.Windows.Forms.Button();
             this.timerLogging = new System.Windows.Forms.Timer(this.components);
-            this.labelVel = new System.Windows.Forms.Label();
+            this.labelStates = new System.Windows.Forms.Label();
             this.nUD_Time = new System.Windows.Forms.NumericUpDown();
             this.labelState = new System.Windows.Forms.Label();
-            this.buttonStop = new System.Windows.Forms.Button();
+            this.labelInfo = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.nUD_Time)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,34 +57,39 @@ namespace AdressLogger
             this.timerMemory.Interval = 10;
             this.timerMemory.Tick += new System.EventHandler(this.timerMemory_Tick);
             // 
-            // buttonLogArm
+            // buttonLogStuff
             // 
-            this.buttonLogArm.Location = new System.Drawing.Point(352, 38);
-            this.buttonLogArm.Name = "buttonLogArm";
-            this.buttonLogArm.Size = new System.Drawing.Size(111, 45);
-            this.buttonLogArm.TabIndex = 1;
-            this.buttonLogArm.Text = "Arm logging";
-            this.buttonLogArm.UseVisualStyleBackColor = true;
-            this.buttonLogArm.Click += new System.EventHandler(this.buttonLog_Click);
+            this.buttonLogStuff.Location = new System.Drawing.Point(12, 250);
+            this.buttonLogStuff.Name = "buttonLogStuff";
+            this.buttonLogStuff.Size = new System.Drawing.Size(100, 36);
+            this.buttonLogStuff.TabIndex = 1;
+            this.buttonLogStuff.Text = "Arm logging";
+            this.buttonLogStuff.UseVisualStyleBackColor = true;
+            this.buttonLogStuff.Click += new System.EventHandler(this.buttonLogStuff_Click);
             // 
             // timerLogging
             // 
             this.timerLogging.Interval = 10000;
             this.timerLogging.Tick += new System.EventHandler(this.timerLogging_Tick);
             // 
-            // labelVel
+            // labelStates
             // 
-            this.labelVel.AutoSize = true;
-            this.labelVel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelVel.Location = new System.Drawing.Point(190, 9);
-            this.labelVel.Name = "labelVel";
-            this.labelVel.Size = new System.Drawing.Size(140, 22);
-            this.labelVel.TabIndex = 0;
-            this.labelVel.Text = "Velocity-lat:";
+            this.labelStates.AutoSize = true;
+            this.labelStates.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStates.Location = new System.Drawing.Point(12, 115);
+            this.labelStates.Name = "labelStates";
+            this.labelStates.Size = new System.Drawing.Size(200, 110);
+            this.labelStates.TabIndex = 0;
+            this.labelStates.Text = "States:\r\nPause \r\nLoads \r\nInv  TF   CS   WinA\r\n0x00 0x00 0x00 0x00";
             // 
             // nUD_Time
             // 
-            this.nUD_Time.Location = new System.Drawing.Point(487, 63);
+            this.nUD_Time.Location = new System.Drawing.Point(118, 260);
+            this.nUD_Time.Maximum = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
             this.nUD_Time.Name = "nUD_Time";
             this.nUD_Time.Size = new System.Drawing.Size(57, 20);
             this.nUD_Time.TabIndex = 2;
@@ -98,37 +103,41 @@ namespace AdressLogger
             // 
             this.labelState.AutoSize = true;
             this.labelState.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelState.Location = new System.Drawing.Point(12, 144);
+            this.labelState.Location = new System.Drawing.Point(12, 289);
             this.labelState.Name = "labelState";
             this.labelState.Size = new System.Drawing.Size(177, 24);
             this.labelState.TabIndex = 0;
             this.labelState.Text = "State: Disconnected";
             // 
-            // buttonStop
+            // labelInfo
             // 
-            this.buttonStop.Location = new System.Drawing.Point(352, 89);
-            this.buttonStop.Name = "buttonStop";
-            this.buttonStop.Size = new System.Drawing.Size(111, 45);
-            this.buttonStop.TabIndex = 1;
-            this.buttonStop.Text = "Stop logging";
-            this.buttonStop.UseVisualStyleBackColor = true;
-            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInfo.Location = new System.Drawing.Point(162, 9);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(60, 88);
+            this.labelInfo.TabIndex = 0;
+            this.labelInfo.Text = "Vel:\r\nnan\r\nHead:\r\nnan";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(576, 177);
+            this.ClientSize = new System.Drawing.Size(245, 323);
             this.Controls.Add(this.nUD_Time);
-            this.Controls.Add(this.buttonStop);
-            this.Controls.Add(this.buttonLogArm);
+            this.Controls.Add(this.buttonLogStuff);
             this.Controls.Add(this.labelState);
-            this.Controls.Add(this.labelVel);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.labelStates);
             this.Controls.Add(this.labelPos);
-            this.Location = new System.Drawing.Point(-600, 500);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Location = new System.Drawing.Point(300, 500);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            this.Text = "Form1";
+            this.Text = "Tallneck At Home:";
+            this.TopMost = true;
             ((System.ComponentModel.ISupportInitialize)(this.nUD_Time)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -139,12 +148,12 @@ namespace AdressLogger
 
         private System.Windows.Forms.Label labelPos;
         private System.Windows.Forms.Timer timerMemory;
-        private System.Windows.Forms.Button buttonLogArm;
+        private System.Windows.Forms.Button buttonLogStuff;
         private System.Windows.Forms.Timer timerLogging;
-        private System.Windows.Forms.Label labelVel;
+        private System.Windows.Forms.Label labelStates;
         private System.Windows.Forms.NumericUpDown nUD_Time;
         private System.Windows.Forms.Label labelState;
-        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label labelInfo;
     }
 }
 
--- AdressLogger/AdressLogger/Form1.cs
+++ AdressLogger/AdressLogger/Form1.cs
@@ -6,69 +6,153 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using LiveSplit.ComponentUtil;
 using System.Diagnostics;
 using System.IO;
 
+
 namespace AdressLogger
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowThreadProcessId(IntPtr handle, out int processId);
+
         public Form1()
         {
             InitializeComponent();
+            myDataPrevBuf.Capacity = 500 / timerMemory.Interval;
+            for (int i = 0; i < myDataPrevBuf.Capacity; ++i)
+            {
+                myDataPrevBuf.Add(new myDataDef());
+            }
         }
 
-        DeepPointer[] ptrs = {
-            new DeepPointer("",0x520,new int[] {0x70,0x120}),
-            new DeepPointer("",0x520,new int[] {0x70,0x128}),
-            new DeepPointer("",0x520,new int[] {0x70,0x130}),
-            new DeepPointer("",0x0714F830,new int[] {0x20})
+
+        // HZD Steam pointers
+        static DeepPointer[] ptrs = {
+            new DeepPointer("", 0x0714cdc8, new int[] {0x30,0x120}),
+            new DeepPointer("", 0x0714cdc8, new int[] {0x30,0x128}),
+            new DeepPointer("", 0x0714cdc8, new int[] {0x30,0x130}),
+            new DeepPointer("", 0x0714cdc8, new int[] {0x30,0x1F0,0x60}),
+            new DeepPointer("", 0x0714F830, new int[] {0x20}),
+            new DeepPointer("", 0x0714F830, new int[] {0x4B4}),
+            new DeepPointer("", 0x0714F830, new int[] {0x158}), // time factor
+            new DeepPointer("", 0x0714F830, new int[] {0x58, 0xA5}), // cutscene?
+            new DeepPointer("", 0x07152640) // tabbed out
         };
-        Process _game = null;
-        string saveFolder = @"C:\OBS\LR-repo\CE-tables\";
-        public struct myPosData
+
+
+        static Process _game = null;
+        string saveFolder = @"C:\Speedrunning-dev\LR-repo\CE-tables\";
+        List<myDataDef> myDataPrevBuf = new List<myDataDef>();
+        List<myDataDef> myData = new List<myDataDef>();
+        string[] tempLbls = new string[3];
+
+        public struct myDataDef
         {
-            public myPosData(double x = Double.NaN, double y = Double.NaN, double z = Double.NaN)
+            public myDataDef(ref Process _game)
             {
                 Time = DateTime.Now;
-                s = 0;
-                PosX = x;
-                PosY = y;
-                PosZ = z;
+                PosX = Double.NaN;
+                PosY = Double.NaN;
+                PosZ = Double.NaN;
+                Velocity = Double.NaN;
+                Heading = Double.NaN;
+                Invulnerable = byte.MaxValue;
+                Pause = uint.MaxValue;
+                Loading = uint.MaxValue;
+                TimeFac = byte.MaxValue;
+                Cutscene = byte.MaxValue;
+                WindowActive = byte.MaxValue;
+                if (_game != null)
+                {
+                    PosX = ptrs[0].Deref<Double>(_game, Double.NaN);
+                    PosY = ptrs[1].Deref<Double>(_game, Double.NaN);
+                    PosZ = ptrs[2].Deref<Double>(_game, Double.NaN);
+                    Invulnerable = ptrs[3].Deref<byte>(_game, byte.MaxValue);
+                    Pause = ptrs[4].Deref<uint>(_game, uint.MaxValue);
+                    Loading = ptrs[5].Deref<uint>(_game, uint.MaxValue);
+                    TimeFac = ptrs[6].Deref<byte>(_game, byte.MaxValue);
+                    Cutscene = ptrs[7].Deref<byte>(_game, byte.MaxValue);
+                    WindowActive = ptrs[8].Deref<byte>(_game, byte.MaxValue);
+                }
             }
-            public myPosData(DateTime time, double x = 0, double y = 0, double z = 0)
+
+            public void calc(myDataDef dataPrev)
             {
-                Time = time;
-                s = 0;
-                PosX = x;
-                PosY = y;
-                PosZ = z;
+                if (valid() && dataPrev.valid())
+                {
+                    double deltaT = (Time.Ticks - dataPrev.Time.Ticks) / ((double)System.TimeSpan.TicksPerSecond);
+                    double dX = PosX - dataPrev.PosX;
+                    double dY = PosY - dataPrev.PosY;
+                    double deltaS = Math.Sqrt(Math.Pow(dX, 2) + Math.Pow(dY, 2) + Math.Pow(PosZ - dataPrev.PosZ, 2));
+                    Velocity = deltaS / deltaT;
+                    if (deltaS > 0.05)
+                    {
+                        Heading = (Math.Atan2(dX, dY) * 180.0 / Math.PI); // This is shifted for geographical heading
+                        if (Heading < 0)
+                        {
+                            Heading += 360.0;
+                        }
+                    }
+                }
             }
+
             public DateTime Time;
-            public double s;
             public double PosX;
             public double PosY;
             public double PosZ;
+            public double Velocity;
+            public double Heading;
+            public byte Invulnerable;
+            public uint Pause;
+            public uint Loading;
+            public byte TimeFac;
+            public byte Cutscene;
+            public byte WindowActive;
             public bool valid()
             {
-                return PosX != 0 && PosY != 0;
+                return (!Double.IsNaN(PosX) && !Double.IsNaN(PosY) && !Double.IsNaN(PosZ) && Loading == 0 && Pause == 0);
             }
             public string ToString(DateTime dateRef)
             {
                 return ToString(dateRef.Ticks);
             }
 
+            public static string LogHeaderFile()
+            {
+                return "t,t_rec,X,Y,Z,vel,head,invul,pause,load,timeMul,cutscene,windowActive";
+            }
+
             public string ToString(long timeRef = 0)
             {
+                // t,t_rec,X,Y,Z,vel,head,invul,pause,load,TF,CS,WindowActive
                 double elapsedSecs = (Time.Ticks - timeRef) / ((double)System.TimeSpan.TicksPerSecond);
-                return elapsedSecs.ToString("0.000") + "," + s.ToString("0.00") + "," + PosX.ToString("0.00") + "," + PosY.ToString("0.00") + "," + PosZ.ToString("0.00");
+                return Time.ToString("yyyy-MM-ddTHH-mm-ss.FFF") + "," + elapsedSecs.ToString("0.000") + "," + PosX.ToString("0.000") + "," + PosY.ToString("0.000") + "," + PosZ.ToString("0.000") + "," + Velocity.ToString("0.000") + "," + Heading.ToString("000") + ",0x" + Invulnerable.ToString("X02") + ",0x" + Pause.ToString("X08") + ",0x" + Loading.ToString("X08") + ",0x" + TimeFac.ToString("X02") + ",0x" + Cutscene.ToString("X02") + ",0x" + WindowActive.ToString("X02");
+            }
+
+            public void WriteToLabels(out string textPos, out string textInfo, out string textStates)
+            {
+                textPos = "Position:\n" + PosX.ToString("0.00") +
+                    "\n" + PosY.ToString("0.00") +
+                    "\n" + PosZ.ToString("0.00");
+                textInfo = "Vel:\n" + Velocity.ToString("0.0") +
+                    "\nHead:\n" + Heading.ToString("000");
+                textStates = "States:\nPause " + Pause.ToString("X08") +
+                    "\nLoads " + Loading.ToString("X08") +
+                    "\nInv  TF   CS   WinA" +
+                    "\n0x" + Invulnerable.ToString("X02") +
+                    " 0x" + TimeFac.ToString("X02") +
+                    " 0x" + Cutscene.ToString("X02") +
+                    " 0x" + WindowActive.ToString("X02");
             }
         }
-        List<myPosData> myData = new List<myPosData>();
-        myPosData posCmp = new myPosData();
-        double totalS = 0;
 
         enum loggingStates
         {
@@ -83,57 +167,31 @@ namespace AdressLogger
         }
         loggingStates logState = loggingStates.Idle;
 
-
-        public double calcLatVel(myPosData prev, myPosData cur)
-        {
-            double value = Double.NaN;
-            if (prev.valid() && cur.valid())
-            {
-                double deltaT = cur.Time.Ticks - prev.Time.Ticks;
-                deltaT /= ((double)System.TimeSpan.TicksPerSecond);
-                double deltaS = Math.Sqrt(calcLatDistSq(prev, cur)); ;
-                if (deltaT > 0)
-                {
-                    value = deltaS / deltaT;
-                }
-            }
-            return value;
-        }
-
-        public double calcLatDistSq(myPosData prev, myPosData cur)
-        {
-            double value = Double.NaN;
-            if (prev.valid() && cur.valid())
-            {
-                value = (cur.PosX - prev.PosX) * (cur.PosX - prev.PosX) + (cur.PosY - prev.PosY) * (cur.PosY - prev.PosY);
-            }
-            return value;
-        }
-
         private void ArmLogging()
         {
             logState = loggingStates.Armed;
             labelState.Text = "State: Armed";
             myData.Clear();
-            myData.Capacity = (int)((nUD_Time.Value + 1) * 1000 / timerLogging.Interval);
+            myData.Capacity = (int)(((nUD_Time.Value + 1) * 1000) / timerMemory.Interval);
             timerLogging.Interval = (int)((nUD_Time.Value) * 1000);
             timerLogging.Stop();
         }
 
         private void StartLogging()
         {
+            buttonLogStuff.Text = "Stop logging";
             logState = loggingStates.Logging;
             labelState.Text = "State: In Progress";
             timerLogging.Start();
         }
 
-        private void AbortLogging()
+        private void StopLogging()
         {
             timerLogging.Stop();
             logState = loggingStates.Idle;
             labelState.Text = "State: Idle";
+            buttonLogStuff.Text = "Arm logging";
             myData.Clear();
-            posCmp = new myPosData();
         }
 
         private void CompleteLogging()
@@ -141,63 +199,61 @@ namespace AdressLogger
             timerLogging.Stop();
             timerMemory.Stop();
             SaveBuffer();
-            AbortLogging();
+            StopLogging();
             timerMemory.Start();
         }
 
         public void SaveBuffer()
         {
-            string fileName = saveFolder + posCmp.Time.ToString("yyyy-MM-ddTHH-mm-ss") + ".csv";
+            if (myData.Count == 0)
+            {
+                return;
+            }
+            if (!System.IO.Directory.Exists(saveFolder))
+            {
+                saveFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\";
+            }
+            string fileName = saveFolder + myData[0].Time.ToString("yyyy-MM-ddTHH-mm-ss") + ".csv";
             StreamWriter sw = new StreamWriter(fileName);
-            sw.WriteLine("t,s,X,Y,Z");
+            sw.WriteLine(myDataDef.LogHeaderFile());
             for (int i = 0; i < myData.Count; ++i)
             {
-                sw.WriteLine(myData[i].ToString(posCmp.Time));
+                sw.WriteLine(myData[i].ToString(myData[0].Time));
             }
             sw.Close();
         }
 
         public void UpdateGameActive()
         {
-            double[] values = new double[3];
-            for (int i = 0; i < 3; ++i)
+            myDataDef myDataCur = new myDataDef(ref _game);
+            myDataCur.calc(myDataPrevBuf[myDataPrevBuf.Count - 1]);
+            for (int i = myDataPrevBuf.Count - 1; i > 0; --i)
             {
-                values[i] = ptrs[i].Deref<double>(_game, Double.NaN);
+                myDataPrevBuf[i] = myDataPrevBuf[i - 1];
             }
-            labelPos.Text = "Position:\n" + values[0].ToString("0.##") +
-                "\n" + values[1].ToString("0.##") +
-                "\n" + values[2].ToString("0.##");
-            myPosData posCur = new myPosData(values[0], values[1], values[2]);
+            myDataPrevBuf[0] = myDataCur;
+            myDataCur.WriteToLabels(out tempLbls[0], out tempLbls[1], out tempLbls[2]);
+            labelPos.Text = tempLbls[0];
+            labelInfo.Text = tempLbls[1];
+            labelStates.Text = tempLbls[2];
 
             switch (logState)
             {
-                case loggingStates.Idle:
+                case loggingStates.Logging:
+                    myData.Add(myDataCur);
                     break;
                 case loggingStates.Armed:
-                    if (!posCmp.valid())
+                    var activatedHandle = GetForegroundWindow();
+                    if (activatedHandle == IntPtr.Zero)
                     {
-                        posCmp = posCur;
+                        break;       // No window is currently activated
                     }
-                    if (calcLatDistSq(posCmp, posCur) > (0.01))
+                    int activeProcID;
+                    GetWindowThreadProcessId(activatedHandle, out activeProcID);
+                    if (_game.Id == activeProcID)
                     {
+                        myData.Add(myDataCur);
                         StartLogging();
-                        myData.Add(posCur);
-                        posCmp = posCur;
-                        totalS = 0;
-                    }
-                    break;
-                case loggingStates.Logging:
-                    double dT = (posCur.Time.Ticks - posCmp.Time.Ticks) / ((double)System.TimeSpan.TicksPerSecond);
-                    double dS = Math.Sqrt(calcLatDistSq(myData[myData.Count - 1], posCur));
-                    totalS += dS;
-                    posCur.s = totalS;
-                    double vel = totalS / dT;
-                    myData.Add(posCur);
-                    labelVel.Text = "Velocity-lat:\n" + vel.ToString("0.##");
-                    bool gamePaused = (ptrs[3].Deref<UInt32>(_game, 0) > 0);
-                    if (gamePaused)
-                    {
-                        CompleteLogging();
                     }
                     break;
                 default:
@@ -209,8 +265,16 @@ namespace AdressLogger
         {
             if (_game == null)
             {
-                Process myProc = Process.GetProcessesByName("HorizonZeroDawn").OrderByDescending(x => x.StartTime)
+                Process myProc = null;
+                try
+                {
+                    myProc = Process.GetProcessesByName("HorizonZeroDawn").OrderByDescending(x => x.StartTime)
                     .FirstOrDefault(x => !x.HasExited);
+                }
+                catch (Exception e)
+                {
+                    myProc = null;
+                }
                 if (myProc == null)
                     return;
                 _game = myProc;
@@ -223,7 +287,7 @@ namespace AdressLogger
                 labelState.Text = "State: Disconnected";
                 if (loggingInProg())
                 {
-                    AbortLogging();
+                    StopLogging();
                 }
             }
             else
@@ -242,14 +306,20 @@ namespace AdressLogger
             CompleteLogging();
         }
 
-        private void buttonLog_Click(object sender, EventArgs e)
+        private void buttonLogStuff_Click(object sender, EventArgs e)
         {
-            ArmLogging();
-        }
-
-        private void buttonStop_Click(object sender, EventArgs e)
-        {
-            AbortLogging();
+            switch (logState)
+            {
+                case loggingStates.Idle:
+                case loggingStates.Armed:
+                    ArmLogging();
+                    break;
+                case loggingStates.Logging:
+                    CompleteLogging();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
